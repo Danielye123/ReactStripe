4b377c1b430815f4657481fe1cf15049d5622f7a
Daniel <daniel_ye.nz@hotmail.com>
2023-07-20 23:00:30 -0700

    setup cards and basic shop

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import {Row, Col} from 'react-bootstrap';
+import { productsArray } from '../productsStore';
+import ProductCard from "../components/ProductCard";
 
 const Store = () => {
-    console.log("you are in the main page")
+
     return (
-    <h1>Welcome to the store!</h1>
+    <>
+        <h1 align="center" className="p-3">Welcome to the store!</h1>
+        <Row  xs={1} md={3} className="g-4">
+            {productsArray.map((product, index) => (
+                <Col align="center" key={index}>
+                    <ProductCard product={product} />
+                </Col>
+            ))}
+        </Row>
+    </>
   )
 }
 
